934d14905ed674a7776f0c9e63ecf88e2dc4b6e7
ITEAM\aluno <marcelo.cruz@iteam.org.br>
2024-07-23 20:30:33 -0400

    Update - Animei o personagem, fiz ele andar de um lado para o outro

--- Scripts/andar.gd
+++ Scripts/andar.gd
@@ -1,20 +1,22 @@
-extends Node2D
+extends Area2D
 
-@export var Velocidade = 2
-@export var possoCorrer = false
+@export var Velocidade = 400
 # Called when the node enters the scene tree for the first time.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(Input.is_action_pressed("right")):
-		position += Vector2(Velocidade, 0)
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("right"):
+		$AnimatedSprite2D.flip_h = false
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		$AnimatedSprite2D.flip_h = true
+		transform.inverse()
+		velocity.x -= 1
 
-
-func correr():
-	possoCorrer =  true
-	pass # Replace with function body.
-
-
-func parar():
-	possoCorrer =  false
-	pass # Replace with function body.
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * Velocidade
+		position += velocity * delta
+		$AnimatedSprite2D.play("walk")
+	else:
+		$AnimatedSprite2D.stop()
